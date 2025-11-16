205f98c60385b14888d301e95795665ab3cb7942
Skylafalls <triplettran56@gmail.com>
2025-11-16 18:30:56 -0500

    add back the fg expansion spreadsheet
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- app/db/docs.ts
+++ app/db/docs.ts
@@ -5,4 +5,15 @@ interface DocumentItem {
   id: string;
 }
 
-export const documents: DocumentItem[] = [];
+export const documents: DocumentItem[] = [
+  {
+    docHeading: "The FG Expansion Spreadsheet",
+    documentLink: "https://docs.google.com/spreadsheets/d/1iYp6VyEhT5JeENpVRjqs7BT9yJd0l-8kHFX_ct7BZMI/edit",
+    arrayOfInfo: [
+      `This is the FG Expansion Spreadsheet, version 2, a spreadsheet that houses many of the numbers of the Fictional Googology community`,
+      `It is meant to be a resource for all things related to FG, such as scales and definitions.`,
+      `Currently in alpha and quite unstable due to weird scaling issues, I ask you help us.`
+    ],
+    id: "fg-exp+bbn"
+  }
+];
